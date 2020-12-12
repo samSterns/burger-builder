63df66e295c9c394ed14a05870144b134be40254
samSterns <sam.g.sterns@gmail.com>
2020-12-11 17:24:24 -0800

    good stopping point ingredients conditionallly render

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,10 +3,25 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])]
+            .map((_, i) => {
+               return  <BurgerIngredient key={igKey + i} type={igKey} />
+            });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+
+        if(transformedIngredients.length === 0) {
+            transformedIngredients = <p>Please Select An Ingredient</p>
+        }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
+        {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     )

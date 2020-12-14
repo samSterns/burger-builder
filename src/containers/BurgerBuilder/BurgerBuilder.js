5b2e04460587fc9041a3742aca02cdee3e979a97
samSterns <sam.g.sterns@gmail.com>
2020-12-14 13:50:25 -0800

    more btn connected

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,13 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+    salad: 0, 
+    avocado: 1.0,
+    cheese: 0.5,
+    mushroom: .7
+}
+
 class BurgerBuilder extends Component {
 
     state = {
@@ -12,13 +19,33 @@ class BurgerBuilder extends Component {
             avocado: 0,
             cheese: 0,
             mushroom: 0
+        },
+        totalPrice: 4
+    }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredients ={
+            ...this.state.ingredients
         }
+        updatedIngredients[type] = updatedCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+    }
+
+    removeIngredientHandler = (type) => {
+
     }
     render () {
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
-                <BuildControls />
+                <BuildControls
+                ingredientAdded={this.addIngredientHandler}
+                />
             </Aux>
         )
     }

7b8bdbaa6682b1aa610856748e38ad5289e1d2ab
samSterns <sam.g.sterns@gmail.com>
2020-12-18 10:45:04 -0800

    error handling and firebase renders ingredients

---
+++ src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import classes from './Spinner.css';
+
+const spinner = () => (
+    <div className={classes.loading}>Loading...</div>
+);
+
+export default spinner;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -7,6 +7,7 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
+import Spinner from '../../components/UI/Spinner/Spinner'
 
 const INGREDIENT_PRICES = {
     salad: 0, 
@@ -26,7 +27,19 @@ class BurgerBuilder extends Component {
         },
         totalPrice: 4,
         purchasable: false,
-        purchasing: false
+        purchasing: false,
+        loading: false,
+        error: false
+    }
+
+    componentDidMount() {
+        axios.get('https://burgerbuilder-edf4b-default-rtdb.firebaseio.com/ingredients.json')
+            .then(response => {
+                this.setState({ingredients: response.data})
+            })
+            .catch(err => {
+                this.setState({error: true})
+            })
     }
 
     updatePurchaseState = (ingredients) => {
@@ -82,6 +95,7 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
        // alert('You continue!')
+       this.setState({loading: true})
        const order = {
            ingredients: this.state.ingredients,
            price: this.state.totalPrice,
@@ -99,8 +113,13 @@ class BurgerBuilder extends Component {
            deliveryMethod: 'fastest'
        }
        axios.post('/orders.json', order)
-       .then(res => console.log(res))
-       .catch(err => console.log(err))
+       .then(response => {
+           this.setState({ loading: false, purchasing: false })
+       })
+
+       .catch(error => {
+        this.setState({ loading: false, purchasing: false })
+       })
     }
 
     render () {
@@ -110,18 +129,16 @@ class BurgerBuilder extends Component {
         for (let key in disableInfo) {
             disableInfo[key] = disableInfo[key] <= 0
         }
-        return (
+
+        let orderSummary = null;
+     
+       
+
+        let burger = this.state.error ? <p>Ingredients cannot be loaded!</p> : <Spinner/>;
+
+        if (this.state.ingredients) {
+            burger = (
             <Aux>
-                <Modal 
-                    show={this.state.purchasing}
-                    modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary
-                        ingredients={this.state.ingredients}
-                        price={this.state.totalPrice}
-                        purchaseCancelled={this.purchaseCancelHandler}
-                        purchaseContinued={this.purchaseContinueHandler}
-                    />
-                </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
                 ingredientAdded={this.addIngredientHandler}
@@ -131,6 +148,27 @@ class BurgerBuilder extends Component {
                 price={this.state.totalPrice}
                 ordered={this.purchaseHandler}
                 />
+            </Aux>
+            );
+            orderSummary = <OrderSummary
+            ingredients={this.state.ingredients}
+            price={this.state.totalPrice}
+            purchaseCancelled={this.purchaseCancelHandler}
+            purchaseContinued={this.purchaseContinueHandler}
+            />
+        }  
+        if(this.state.loading) {
+            orderSummary = <Spinner />
+        }
+        return (
+            <Aux>
+                <Modal 
+                    show={this.state.purchasing}
+                    modalClosed={this.purchaseCancelHandler}>
+                    {orderSummary}
+                     
+                </Modal>
+                {burger}
             </Aux>
         )
     }
